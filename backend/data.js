8ed5fa1bb7d9a0dc7a00c52dacd208c323c78a31
andergcp <andergcp@hotmail.com>
2020-11-10 15:58:41 -0500

    Setting up backend

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            countInStock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Lacoste Slim Shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:140,
+            countInStock:12,
+            brand:'Lacoste',
+            rating:4.8,
+            numReviews:4,
+            description:'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Polo Slim Shirt',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:120,
+            countInStock:0,
+            brand:'Polo',
+            rating:4.2,
+            numReviews:11,
+            description:'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Koaj Slim Pant',
+            category:'Shirts',
+            image:'/images/p4.jpg',
+            price:120,
+            countInStock:15,
+            brand:'Koaj',
+            rating:3.8,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Ralph Slim Pant',
+            category:'Shirts',
+            image:'/images/p5.jpg',
+            price:120,
+            countInStock:5,
+            brand:'Ralph',
+            rating:4.8,
+            numReviews:2,
+            description:'high quality product'
+        },
+        {
+            _id:'6',
+            name:'Abrand Slim Pant',
+            category:'Shirts',
+            image:'/images/p6.jpg',
+            price:120,
+            countInStock:12,
+            branc:'Abrand',
+            rating:4.1,
+            numReviews:16,
+            description:'high quality product'
+        }
+    ]
+}
+
+export default data;
